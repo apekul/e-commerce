f78f8653b5af85fa7fca1cd8c6e3a06eab0d4456
Adaś <adampekul@gmail.com>
2023-10-17 19:47:57 +0200

    add filter style to allProductPage

---
+++ src/Components/priceRange.js
@@ -0,0 +1,73 @@
+import React from "react";
+import Slider from "@mui/material/Slider";
+import MuiInput from "@mui/material/Input";
+
+const PriceRange = ({ value }) => {
+  const [range, setRange] = React.useState([value.min, value.max]);
+  function handleChanges(event, newValue) {
+    setRange(newValue);
+  }
+  const handleInputChange = (event, val) => {
+    // if (event.target.value === "") event.target.value = 0;
+    if (val === "min") {
+      return setRange(([min, max]) => [event.target.value, max]);
+    }
+    setRange(([min, max]) => [min, event.target.value]);
+  };
+
+  const handleBlur = () => {
+    if (range[0] < 0) {
+      setRange(([min, max]) => [value.min, max]);
+    } else if (range[1] > 100) {
+      setRange(([min, max]) => [min, value.max]);
+    }
+  };
+  return (
+    <div style={{ width: "150px", padding: "20px" }}>
+      <div className="PriceRangeGroup">
+        <span>
+          $
+          <MuiInput
+            value={range[0]}
+            size="small"
+            onChange={(event) => handleInputChange(event, "min")}
+            onBlur={handleBlur}
+            inputProps={{
+              step: 10,
+              min: value.min,
+              max: value.max,
+              type: "number",
+              "aria-labelledby": "input-slider",
+            }}
+          />
+        </span>
+        <span>-</span>
+        <span>
+          $
+          <MuiInput
+            value={range[1]}
+            size="small"
+            onChange={(event) => handleInputChange(event, "max")}
+            onBlur={handleBlur}
+            inputProps={{
+              step: 10,
+              min: value.min,
+              max: value.max,
+              type: "number",
+              "aria-labelledby": "input-slider",
+            }}
+          />
+        </span>
+      </div>
+      <Slider
+        value={range}
+        onChange={handleChanges}
+        max={value.max}
+        min={value.min}
+      />
+      <button onClick={() => setRange([value.min, value.max])}>Reset</button>
+    </div>
+  );
+};
+
+export default PriceRange;
--- src/Pages/AllProductsPage.js
+++ src/Pages/AllProductsPage.js
@@ -1,7 +1,110 @@
-import React from "react";
+import React, { useState, useContext, useRef } from "react";
+import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
+import { BsCheck, BsSortDownAlt, BsSortUpAlt } from "react-icons/bs";
+import { Context, UserContext } from "../context";
+import PriceRange from "../Components/priceRange";
 
 const AllProductsPage = () => {
-  return <div>AllProductsPage</div>;
+  const [data, setData] = useContext(Context);
+  const [show, setShow] = useState(undefined);
+  const [type, setType] = useState([]);
+  const [range, setRange] = useState();
+
+  const PriceRangeRef = useRef();
+
+  const updateType = (e) => {
+    if (type.includes(e))
+      return setType((prev) => prev.filter((item) => item !== e));
+    setType((prev) => [...prev, e]);
+  };
+
+  const updateShow = (e) => {
+    const { id } = e.currentTarget;
+    if (show === id) return setShow(undefined);
+    setShow(id);
+  };
+
+  const categories = [...new Set(data.map((v, i) => v.category))];
+
+  return (
+    <section id="allProductsPage">
+      <div className="Container">
+        <div className="filterPanelGroup">
+          {/* Filter by type */}
+          <div className="filterButtonGroup">
+            <div
+              id="filterType"
+              className="filterButton"
+              onClick={(e) => updateShow(e)}
+            >
+              <p>Type</p>
+              <div
+                className="filterMark"
+                style={{ visibility: type.length > 0 ? "visible" : "hidden" }}
+              >
+                {type.length}
+              </div>
+              {show === "filterType" ? <AiFillCaretUp /> : <AiFillCaretDown />}
+            </div>
+            {show === "filterType" && (
+              <ul className="filterList typeBefore">
+                {categories.map((category, index) => (
+                  <li key={index} onClick={() => updateType(category)}>
+                    <p>{category}</p>
+                    {type.includes(category) && <BsCheck size="20px" />}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+          {/* Filter by Price */}
+          <div className="filterButtonGroup">
+            <div
+              id="filterPrice"
+              className="filterButton"
+              onClick={(e) => updateShow(e)}
+            >
+              <p>Price</p>
+              <div
+                className="filterMark"
+                style={{ visibility: range ? "visible" : "hidden" }}
+              >
+                1
+              </div>
+              {show === "filterPrice" ? <AiFillCaretUp /> : <AiFillCaretDown />}
+            </div>
+            <div
+              ref={PriceRangeRef}
+              className="filterList priceBefore"
+              style={{
+                visibility: show === "filterPrice" ? "visible" : "hidden",
+              }}
+            >
+              <PriceRange value={{ min: 100, max: 1000 }} />
+            </div>
+          </div>
+          {/* Filter by Rate */}
+          <div className="filterButtonGroup">
+            <div
+              id="filterRate"
+              className="filterButton"
+              onClick={(e) => updateShow(e)}
+            >
+              <p>Rate</p>
+              <div
+                className="filterMark"
+                style={{ visibility: range ? "visible" : "hidden" }}
+              >
+                1
+              </div>
+              {show === "filterRate" ? <BsSortUpAlt /> : <BsSortDownAlt />}
+            </div>
+          </div>
+        </div>
+        <div>Content Items</div>
+      </div>
+    </section>
+  );
 };
 
 export default AllProductsPage;
